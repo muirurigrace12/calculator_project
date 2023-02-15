548b44ee59dd52d3f6e82424e97fd448bec747a1
muirurigrace12 <muirurigrace33@gmail.com>
2023-02-15 11:38:20 +0300

    added error checking

--- calculator.py
+++ calculator.py
@@ -7,16 +7,35 @@ operator = input("enter operator")
 
 def add(number1, number2):
     result = number1 + number2
-    return result
+    print(result)
 
 def subtract(number1, number2):
     result = number1 - number2
-    return result
+    print(result)
 
 def divide(number1, number2):
     result = number1 / number2
-    return result
+    print(result)
 
 def multiply(number1, number2):
     result = number1 * number2
-    return result
+    print(result)
+
+#check operator
+if operator == "+":
+    add(number1,number2)
+
+elif operator == "/":
+    divide(number1, number2)
+
+elif operator == "-":
+    subtract(number1, number2)
+
+elif operator == "X" or operator == "*":
+    multiply(number1, number2)
+
+else:
+    print("invalid operator")
+    
+
+
